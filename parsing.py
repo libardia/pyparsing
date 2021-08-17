08b1e0806a2b4fed12f8c005adfb68385c414cd9
Anthony Libardi <libardi42@gmail.com>
2021-08-17 12:06:01 -0500

    These all have to be static functions, not tied to objects, so that parsers can be built recursively. Now it's actual proper python with the static method annotations.

--- parsing.py
+++ parsing.py
@@ -6,16 +6,19 @@ class ResultProcessors:
     """Functions for taking results of many parsers (such as from `Combinators.chain` or `Combinators.many`) and combining into one result,
     or any other post processing you wish to do on the result of a parser (for use with `Combinators.after`)."""
 
+    @staticmethod
     def doNothing(rs):
         """Result processor: takes a list of results from multiple parsers and combines them into a single result.\n
         No-op; returns exactly what was given."""
         return rs
 
+    @staticmethod
     def concat(rs):
         """Result processor: takes a list of results from multiple parsers and combines them into a single result.\n
         Converts results to strings and concatenates them."""
         return ''.join(str(r) for r in rs)
 
+    @staticmethod
     def take(*indexes):
         """Result processor generator: returns a result processor: ``func(results) -> result``\n
         Ignores all results except those at the given indexes. Result is a tuple, unless only one item is taken."""
@@ -26,6 +29,7 @@ class ResultProcessors:
             return result
         return fromProcTake
 
+    @staticmethod
     def print(proc):
         """Result processor generator: returns a result processor: ``func(results) -> result``\n
         Returns a processor that prints the results it recieves before calling a different processor. Useful for debugging."""
@@ -41,6 +45,7 @@ class PrimitiveParsers:
 
     # Ultra-basic: decides to take a character based on a function on that character.
     # Used for pretty much all the other primitive parsers.
+    @staticmethod
     def byFunc(f):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that that will parse a single character from the input string according to the function provided. Provided function must
@@ -53,36 +58,43 @@ class PrimitiveParsers:
 
     # Primitives
 
+    @staticmethod
     def digit(string):
         """Parser: takes a string as an input and returns either ``None`` if the parser failed, or a tuple of the parsed output and the remaining unconsumed input.\n
         Parses a single digit (0-9) from the input string."""
         return PrimitiveParsers.byFunc(lambda c: c.isdigit())(string)
 
+    @staticmethod
     def letter(string):
         """Parser: takes a string as an input and returns either ``None`` if the parser failed, or a tuple of the parsed output and the remaining unconsumed input.\n
         Parses a single letter (a-z or A-Z) from the input string."""
         return PrimitiveParsers.byFunc(lambda c: c.isalpha())(string)
 
+    @staticmethod
     def nonWhitespace(string):
         """Parser: takes a string as an input and returns either ``None`` if the parser failed, or a tuple of the parsed output and the remaining unconsumed input.\n
         Parses a single non-whitespace character from the input string."""
         return PrimitiveParsers.byFunc(lambda c: not c.isspace() and c != '')(string)
 
+    @staticmethod
     def whitespace(string):
         """Parser: takes a string as an input and returns either ``None`` if the parser failed, or a tuple of the parsed output and the remaining unconsumed input.\n
         Parses a single whitespace character from the input string."""
         return PrimitiveParsers.byFunc(lambda c: c.isspace())(string)
 
+    @staticmethod
     def char(c):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that that will parse one of the given character from the input string."""
         return PrimitiveParsers.byFunc(lambda ic: ic == c)
-    
+
+    @staticmethod
     def notChar(c):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that that will parse any character except the given character from the input string."""
         return PrimitiveParsers.byFunc(lambda ic: ic != c and ic != '')
 
+    @staticmethod
     def take(n):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that that will parse `n` characters from the input string, whatever they are."""
@@ -91,6 +103,7 @@ class PrimitiveParsers:
                 return string[:n], string[n:]
         return fromTake
 
+    @staticmethod
     def reg(pattern):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that that will parse and return the match from the given regex expression. It will always consume and return all
@@ -105,6 +118,7 @@ class PrimitiveParsers:
 # Combinators
 
 class Combinators:
+    @staticmethod
     def chain(*parsers, proc=ResultProcessors.concat):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that attempts to runs a list of parsers one at a time, and only succeeds if all of them succeed."""
@@ -123,6 +137,7 @@ class Combinators:
             return results, rest
         return Combinators.after(fromChain, proc)
 
+    @staticmethod
     def many(parser, proc=ResultProcessors.concat):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that runs the same parser over and over until it fails, and returns all results. Fails if it can't parse at
@@ -139,11 +154,13 @@ class Combinators:
             return results, rest
         return Combinators.after(fromMany, proc)
 
+    @staticmethod
     def manyOrNone(parser, proc=ResultProcessors.concat):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that runs the same parser over and over until it fails, and returns all zero or more results."""
         return Combinators.maybe(Combinators.many(parser, proc))
 
+    @staticmethod
     def maybe(parser):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that runs one parser and never fails; if the given parser fails, this parser returns an empty string. Otherwise
@@ -156,6 +173,7 @@ class Combinators:
                 return pr
         return fromMaybe
 
+    @staticmethod
     def choice(*parsers):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that runs through a list of given parsers one at a time until one succeeds, and returns that result. Fails if
@@ -167,6 +185,7 @@ class Combinators:
                     return pr
         return fromChoice
 
+    @staticmethod
     def ignore(parser):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that runs the given parser, but then returns an empty string, no matter what the given parser did. This still consumes
@@ -179,6 +198,7 @@ class Combinators:
                 return '', pr[1]
         return fromIgnore
 
+    @staticmethod
     def after(parser, proc):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that runs a parser, and then runs the given result processor on the result. (NOTE: passed into the processor will
@@ -195,6 +215,7 @@ class Combinators:
                     return None
         return fromAfter
 
+    @staticmethod
     def whole(parser):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that fails if the given parser does not consume the entire input, but otherwise behaves the same as the input parser."""
@@ -203,7 +224,8 @@ class Combinators:
             if result is not None and result[1] == '':
                 return result
         return fromWhole
-    
+
+    @staticmethod
     def conclude(parser):
         """Parser generator: returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that, uniquely, only returns the result and not the unconsumed input. Used for finishing a large, complex parser, so the end user only recieves the parsed object."""
@@ -218,6 +240,7 @@ class Combinators:
 class PrebuiltParsers:
     """Premade parsers composed of other parsers. These are provided for convenience, but also as examples of how to combine parsers together."""
 
+    @staticmethod
     def restOfLine(string):
         """Parser: takes a string as an input and returns either ``None`` if the parser failed, or a tuple of the parsed output and the remaining unconsumed input.\n
         Consumes all input until and including a newline character, or EOF. This parser is composed as follows::
@@ -229,7 +252,8 @@ class PrebuiltParsers:
             Combinators.manyOrNone(PrimitiveParsers.notChar('\n')),
             Combinators.maybe(PrimitiveParsers.char('\n'))
         )(string)
-    
+
+    @staticmethod
     def quotedString(string):
         """Parser: takes a string as an input and returns either ``None`` if the parser failed, or a tuple of the parsed output and the remaining unconsumed input.\n
         Consumes all input within two double quotes, ignoring escaped quotes. This parser is composed as follows::
@@ -253,7 +277,8 @@ class PrebuiltParsers:
 
             proc=lambda rs: rs[1].replace('\\"', '"')
         )(string)
-    
+
+    @staticmethod
     def singleQuotedString(string):
         """Parser: takes a string as an input and returns either ``None`` if the parser failed, or a tuple of the parsed output and the remaining unconsumed input.\n
         Consumes all input within two single quotes, ignoring escaped quotes. This parser is composed as follows::
@@ -278,6 +303,7 @@ class PrebuiltParsers:
             proc=lambda rs: rs[1].replace('\\\'', '\'')
         )(string)
 
+    @staticmethod
     def isw(parser):
         """Returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Short for "ignore surrounding whitespace." Returns a parser that ignores all whitespace before and after the input parser, but only
@@ -297,6 +323,7 @@ class PrebuiltParsers:
             proc = ResultProcessors.take(1)
         )
 
+    @staticmethod
     def prefix(pre):
         """Returns parser as a function: ``func(string) -> tuple(result, rest) or None``\n
         Returns a parser that consumes and returns the given prefix from the beginning of the input. This parser is composed as follows::
@@ -313,6 +340,7 @@ class PrebuiltParsers:
             )(string)
         return fromPrefix
 
+    @staticmethod
     def token(string):
         """Parser: takes a string as an input and returns either ``None`` if the parser failed, or a tuple of the parsed output and the remaining unconsumed input.\n
         Consumes and returns all non-whitespace characters from the beginning of the input. This parser is composed as follows::
@@ -321,6 +349,7 @@ class PrebuiltParsers:
             )"""
         return Combinators.many(PrimitiveParsers.nonWhitespace)(string)
 
+    @staticmethod
     def allWhitespace(string):
         """Parser: takes a string as an input and returns either ``None`` if the parser failed, or a tuple of the parsed output and the remaining unconsumed input.\n
         Parses as many whitespase characters as possbile. This parser is composed as follows::
@@ -329,6 +358,7 @@ class PrebuiltParsers:
             )"""
         return Combinators.manyOrNone(PrimitiveParsers.whitespace)(string)
 
+    @staticmethod
     def integer(string):
         """Parser: takes a string as an input and returns either ``None`` if the parser failed, or a tuple of the parsed output and the remaining unconsumed input.\n
         Parses an integer. This parser is composed as follows::
@@ -344,7 +374,8 @@ class PrebuiltParsers:
 
             proc = lambda rs: int(''.join(rs))
         )(string)
-    
+
+    @staticmethod
     def decmial(string):
         """Parser: takes a string as an input and returns either ``None`` if the parser failed, or a tuple of the parsed output and the remaining unconsumed input.\n
         Parses a decimal number (float). This parser is composed as follows::
